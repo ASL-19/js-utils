40348a66d7a07cd9ca70e9e3f769d0aa792b1734
Grant Heaslip <grant@asl19.org>
2020-07-27 16:45:23 -0400

    Upgrade @asl-19/eslint-config to 0.2.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,8 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 const eslintConfig = {
   extends: ["@asl-19/eslint-config"],
-  plugins: ["eslint-plugin-tsdoc"],
   rules: {
     "no-restricted-imports": "off",
-    "tsdoc/syntax": "warn",
   },
 };
 
