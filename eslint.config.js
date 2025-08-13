4e5e97cd7ac784a05b00d45155944934a5602230
Grant Heaslip <grant@asl19.org>
2025-08-13 18:46:39 -0400

    Remove @asl-19/shared-package-configs, update configs, misc. cleanup:
    - Removed remaining references to obsolete types directory (types now
    come from .d.ts files in dist)
    
    - tsconfig.json no longer extends from
    @asl-19/shared-package-configs/tsconfig-base.json. Inlined the content
    of @asl-19/shared-package-configs/tsconfig-base.json and modernized
    some configs. Also note that tsconfig-esm.json was renamed to
    tsconfig-dist.json, and it extends our tsconfig.json.
    
    - rm-dist now removes all files in dist (rather than the directory
    itself), causing less disruption during builds
    
    - Added new dev-build-update script for development
    
    - Typed prettier.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,16 @@
 // We should be able to remove the @ts-expect-error and eslint-disable-next-line
 // once we fix the typing of @asl-19/eslint-config
 
-// @ts-expect-error (TODO: why is this happening?)
 import asl19 from "@asl-19/eslint-config";
 import { defineConfig } from "eslint/config";
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const typedAsl19 =
-  /** @type {import("@asl-19/eslint-config/types/index")["default"]} */ (asl19);
+const typedAsl19 = /** @type {import("@asl-19/eslint-config")["default"]} */ (
+  asl19
+);
 
 const eslintConfig = defineConfig([
   {
-    ignores: ["dist/", "types/", ".yalc/"],
+    ignores: ["dist/", ".yalc/"],
   },
   {
     extends: [
